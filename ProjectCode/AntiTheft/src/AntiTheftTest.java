03a505cbdbde66758cb14e2006eda5c4d88589af
vp7273 <39107473+vp7273@users.noreply.github.com>
2019-10-30 20:31:49 -0500

    eclipse testing changes from 10/30 meeting

--- ProjectCode/AntiTheft/src/AntiTheftTest.java
+++ ProjectCode/AntiTheft/src/AntiTheftTest.java
@@ -1,8 +1,11 @@
 // This is the class for setting up entire application
 // Coded by: Christopher Guerra
+// Coded by: Varika Pinnam
 public class AntiTheftTest {
 
 	public static void main(String[] args) {
+		System.out.println("Run");
+		System.out.print("Testing eclipse");
 		
 	}
 
