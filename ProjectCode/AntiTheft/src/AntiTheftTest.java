54ee934a7ec8f371d7f916c8217354f400f2a1be
vp7273 <39107473+vp7273@users.noreply.github.com>
2019-11-10 19:14:48 -0600

    user

--- ProjectCode/AntiTheft/src/AntiTheftTest.java
+++ ProjectCode/AntiTheft/src/AntiTheftTest.java
@@ -3,21 +3,29 @@
 //Test comment by: Sindhura Boppudi
 // >>>>>>> 623c8dea3783e2084652f0f577a4307fdb260c87
 import java.util.Scanner;
+import java.util.Vector;
+import java.util.HashMap;
+
 
 public class AntiTheftTest {
+	
+	  Vector <Integer> numbers = new Vector <Integer> ();
+
+	  HashMap<String, Integer> database = new HashMap<>();
+
+	  HashMap<String, String> password = new HashMap <>();
+
+	  static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		System.out.println("Run");
 		System.out.print("Testing eclipse");
-		// Noman Syed
-		//varika
+	
 	}
 
 
 
 	public static void LookUp () {
-
-	  Scanner input = new Scanner(System.in);
 
 	  System.out.print("Enter your assigned user ID to track this item");
 
@@ -26,8 +34,18 @@ public class AntiTheftTest {
 	  input.close();
 	}
 
-	private static void assignID () {
+	
+  //assigning account number in database
+
+    public static void assignID (){
+
+    	int number;
+    	
+    	number = 
+        System.out.print("Your number is" + number);
+
+  
+  }
 
 
-	}
 }
