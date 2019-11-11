9b14e51005f0592725346a1d17c99074de54c044
SindhuraB <38262408+SindhuraB@users.noreply.github.com>
2019-11-10 18:30:32 -0600

    UI for Opening page and Sign in Page

---
+++ ProjectCode/AntiTheft/src/signIn.java
@@ -0,0 +1,72 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+public class signIn {
+
+	private JFrame frame;
+	private JTextField txtEnterUsernameHere;
+	private JTextField txtEnterPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					signIn window = new signIn();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public signIn() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 404, 287);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnLogIn = new JButton("Log In");
+		btnLogIn.setBounds(152, 172, 89, 23);
+		frame.getContentPane().add(btnLogIn);
+		
+		txtEnterUsernameHere = new JTextField();
+		txtEnterUsernameHere.setText("Enter Username Here");
+		txtEnterUsernameHere.setBounds(175, 76, 143, 23);
+		frame.getContentPane().add(txtEnterUsernameHere);
+		txtEnterUsernameHere.setColumns(10);
+		
+		txtEnterPassword = new JTextField();
+		txtEnterPassword.setText("Enter Password");
+		txtEnterPassword.setBounds(175, 107, 143, 23);
+		frame.getContentPane().add(txtEnterPassword);
+		txtEnterPassword.setColumns(10);
+		
+		JTextPane txtpnUsername = new JTextPane();
+		txtpnUsername.setText("Username: ");
+		txtpnUsername.setBounds(69, 76, 81, 23);
+		frame.getContentPane().add(txtpnUsername);
+		
+		JTextPane txtpnPassword = new JTextPane();
+		txtpnPassword.setText("Password");
+		txtpnPassword.setBounds(69, 110, 81, 20);
+		frame.getContentPane().add(txtpnPassword);
+	}
+}
