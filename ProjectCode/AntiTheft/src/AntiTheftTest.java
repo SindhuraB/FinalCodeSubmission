0b2ea16a2301708bfbbbf63d4e3c2c7707872c7c
vp7273 <39107473+vp7273@users.noreply.github.com>
2019-11-10 18:43:36 -0600

    change

--- ProjectCode/AntiTheft/src/AntiTheftTest.java
+++ ProjectCode/AntiTheft/src/AntiTheftTest.java
@@ -2,22 +2,31 @@
 // Coded by: Christopher Guerra, Varika Pinnam
 //Test comment by: Sindhura Boppudi
 // >>>>>>> 623c8dea3783e2084652f0f577a4307fdb260c87
+import java.util.Scanner;
+
 public class AntiTheftTest {
 
 	public static void main(String[] args) {
 		System.out.println("Run");
 		System.out.print("Testing eclipse");
-		
+
 	}
 
 
-}
 
-public class LookUp {
-	
+	public static void LookUp () {
+
+	  Scanner input = new Scanner(System.in);
+
 	  System.out.print("Enter your assigned user ID to track this item");
-	  
-	  String id= input.next;
-	  
-	
+
+	  String id= input.next();
+
+	  input.close();
+	}
+
+	private static void assignID () {
+
+
+	}
 }
