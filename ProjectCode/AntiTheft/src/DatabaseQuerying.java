95b3bd5f578937599b09f577321c366fe9af133e
labeebarana <56279361+labeebarana@users.noreply.github.com>
2019-11-12 15:07:04 -0600

    Creating database connection

--- ProjectCode/AntiTheft/src/DatabaseQuerying.java
+++ ProjectCode/AntiTheft/src/DatabaseQuerying.java
@@ -2,4 +2,25 @@ import java.sql.*;
 
 public class DatabaseQuerying {
 
+	public static void main(String args[]) {
+		try {
+			//load the driver class
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//create the connection object
+			Connection con = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:orclpdb", "TAGCOMPDBA", "minigrr1");
+			//create the statement object 
+			Statement stmt = con.createStatement();
+			//execute query; update the '1' to variable input from GUI
+			ResultSet rs = stmt.executeQuery("select * from USER_TAGS where ACCOUNTID = '1'");
+			//close the connection object
+			con.close();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	
 }
