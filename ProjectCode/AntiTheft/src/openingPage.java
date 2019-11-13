5e2d88827b1b02e286cb9a9ba1fd49ccbc9853ad
Amiel Vincent <56279501+avincent428@users.noreply.github.com>
2019-11-13 17:00:29 -0600

    Added comments
    Amiel Vincent and Zareef Kabir added comments for clarity

--- ProjectCode/AntiTheft/src/openingPage.java
+++ ProjectCode/AntiTheft/src/openingPage.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 
+// opening page class
 public class openingPage {
 
 	private JFrame frame;
@@ -11,9 +12,9 @@ public class openingPage {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {	// main method
 		EventQueue.invokeLater(new Runnable() {
-			public void run() {
+			public void run() {	// run opening page
 				try {
 					openingPage window = new openingPage();
 					window.frame.setVisible(true);
@@ -35,15 +36,18 @@ public class openingPage {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		// creates window frame
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		// Sign in button
 		JButton btnSignIn = new JButton("Sign In");
 		btnSignIn.setBounds(163, 92, 89, 23);
 		frame.getContentPane().add(btnSignIn);
 		
+		// Sign up button
 		JButton btnSignUp = new JButton("Sign Up");
 		btnSignUp.setBounds(163, 128, 89, 23);
 		frame.getContentPane().add(btnSignUp);
