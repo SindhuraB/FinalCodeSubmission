6fb4337d7ba1b9abab155c45b689b7a02305d56b
ChrisJamesG <boss2themax@gmail.com>
2019-11-28 21:44:28 -0600

    Added query return string parsing and updated QueryProtocol with request parsing and added to request options

--- ProjectCode/AntiTheft/src/QueryMethods.java
+++ ProjectCode/AntiTheft/src/QueryMethods.java
@@ -21,8 +21,8 @@ public class QueryMethods extends DatabaseQuerying{
 	}
 	public static String login(String email, String pass)
 	{
-		String request = "select a.AcID from ACCOUNTS a where a.Email = " + email
-				+ " and a.Password = " + pass + "0";
+		String request = "select a.AcID from ACCOUNTS a where a.Email = '" + email
+				+ "' and a.Password = '" + pass + "'0";
 		String validity = makeRequest(request);
 		return validity;
 	}
@@ -30,12 +30,28 @@ public class QueryMethods extends DatabaseQuerying{
 	{
 		String request = "select u.Brand, u.ItemDesc from USER_TAGS u where u.AccountID = " + accID + "1";
 		String products = makeRequest(request);
+		products = products.replace("|", "\n");
 		return products;
 	}
-	public static String getUserInfo(String accID)
+	public static String[] getUserInfo(String accID)
 	{
-		String request = "select * from USER_INFO u where u.AccountID = " + accID + "2";
-		String userInfo = makeRequest(request);
+		String request = "select * from USER_INFO u where u.AccountID = " + accID + 
+				"|" + "select * from ACCOUNTS a where a.AcID = " + accID + "2";
+		String infoString = makeRequest(request);
+		System.out.println("Length: " + infoString.length() + "\n" + infoString);
+		String userInfo[] = new String[11];
+		int previousSubstringEnd = 0;
+		int count = 0;
+		for(int i = 0; i < infoString.length(); i++)
+		{
+			if(infoString.charAt(i) == '|')
+			{
+				userInfo[count] = infoString.substring(previousSubstringEnd, i);
+				previousSubstringEnd = i + 1;
+				count++;
+			}
+		}
+		
 		return userInfo;
 	}
 	public static String createUser(String email, String pass, String fName, String mInit, String lName, String street,
@@ -45,7 +61,7 @@ public class QueryMethods extends DatabaseQuerying{
 		// Create database querying object
 		String columns = "Fname, Minit, Lname, Streetname, City, State, ZipCode, Phone, Extension, AccountID";
 		String request = "insert into ACCOUNT (AcID, Email, Password) "
-				+ "values ('" + newID + "', '" + email + "', '" + pass + "')|"
+				+ "values ('" + newID + "', '" + email + "', '" + pass + "')" + "|"
 				+"insert into TAGUSER ("+ columns + ") "
 				+ "values ('" + fName + "', '" + mInit + "', '" + lName + "', '" + street + "', '" + city 
 				+ "', '" + state + "', '" + zipCode + "', '" + phone + "', '" + extension + "', '" + newID + "')3";
@@ -53,8 +69,20 @@ public class QueryMethods extends DatabaseQuerying{
 		return results;
 	}
 	public static void main(String[] args) {
-		String valid = selectProducts("1");
-		System.out.println(valid);
+		String newID = getNewID();
+		System.out.println(newID);
+		String validLogin = login("Cool@cool.com", "Cool123");
+		System.out.println("Valid login: " + validLogin);
+		String invalidLogin = login("Cool@cool.com", "Cool12");
+		System.out.println("Invalid login: " + invalidLogin);
+		String products = selectProducts("1");
+		System.out.println("Products: /n" + products);
+		String[] userInfo = new String[11];
+		userInfo = getUserInfo("1");
+		for(int i = 0; i < userInfo.length; i++)
+			System.out.print(userInfo[i] + " ");
+		System.out.println("");
 		String shut = shutdownServer();
+		System.out.println("Server shutdown");
 	}
 }
--- ProjectCode/AntiTheft/src/QueryingProtocol.java
+++ ProjectCode/AntiTheft/src/QueryingProtocol.java
@@ -53,6 +53,24 @@ public class QueryingProtocol {
 			System.out.println("Querying:");
 			System.out.println(sql);
 			
+			// Get ID from account relating to email and password
+			try
+			{
+				//create the statement object
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				System.out.println("Query Executed");
+				if(rs.next())
+				{
+					resultString = rs.getString("AcID");
+					System.out.println("Row results: " + resultString);
+				}
+				con.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
 		}
 		else if(requestCode == productsSQL)
 		{
@@ -68,10 +86,11 @@ public class QueryingProtocol {
 				{
 					String brand = rs.getString("Brand");
 					String desc = rs.getString("ItemDesc");
-					String row = brand + ", " + desc;
-					resultString = row;
+					String row = brand + ", " + desc + "|";
+					resultString = resultString.concat(row);
 					System.out.println("Row results: " + row);
 				}
+				System.out.println("All products: " + resultString);
 				con.close();
 			}
 			catch(Exception e)
@@ -82,14 +101,100 @@ public class QueryingProtocol {
 		else if(requestCode == userInfoSQL)
 		{
 			System.out.println("Querying:");
-			System.out.println(sql);
 			
+			String userInfoSQL = "";
+			String accInfoSQL = "";
+			for(int i = 0; i < sql.length() - 1; i++)
+			{
+				if(sql.charAt(i) == '|')
+				{
+					userInfoSQL = sql.substring(0, i);
+					System.out.println(userInfoSQL);
+					accInfoSQL = sql.substring(i + 1, sql.length());
+					System.out.println(accInfoSQL);
+					break;
+				}
+			}
+			
+			try
+			{
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(userInfoSQL);
+				System.out.println("User Query Executed");
+				// Initialize result strings
+				String fName = "";
+				String mInit = "";
+				String lName = "";
+				String streetName = "";
+				String city = ""; 
+				String state = "";
+				String zipCode = "";
+				String phone = "";
+				String extension = "";
+				String email = "";
+				String pass = "";
+				if(rs.next())
+				{
+					fName = rs.getString("Fname");
+					mInit = rs.getString("Minit");
+					lName = rs.getString("Lname");
+					streetName = rs.getString("StreetName");
+					city = rs.getString("City");
+					state = rs.getString("State");
+					zipCode = rs.getString("ZipCode");
+					phone = rs.getString("Phone");
+					extension = rs.getString("Extension");
+				}
+				rs = stmt.executeQuery(accInfoSQL);
+				System.out.println("Account Query Executed");
+				if(rs.next())
+				{
+					email = rs.getString("Email");
+					pass = rs.getString("Password");
+				}
+				String parseableResults = fName + "|" + mInit + "|" + lName + "|" + streetName + "|" + city + "|" + state
+						+ "|" + zipCode + "|" + phone + "|" + extension + "|" + email + "|" + pass + "|";
+				resultString = parseableResults;
+				System.out.println("All products: " + resultString);
+				con.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
 		}
 		else if(requestCode == createUserSQL)
 		{
 			System.out.println("Querying:");
-			System.out.println(sql);
 			
+			String userUpdateSQL = "";
+			String accUpdateSQL = "";
+			for(int i = 0; i < sql.length() - 1; i++)
+			{
+				if(sql.charAt(i) == '|')
+				{
+					userUpdateSQL = sql.substring(0, i - 1);
+					accUpdateSQL = sql.substring(i + 1, sql.length() - 1);
+				}
+			}
+			System.out.println(userUpdateSQL);
+			System.out.println(accUpdateSQL);
+			
+			// create user
+			try
+			{
+				//create the statement object
+				Statement stmt = con.createStatement();
+				int rsUser = stmt.executeUpdate(userUpdateSQL);
+				int rsAcc = stmt.executeUpdate(accUpdateSQL);
+				System.out.println("Query Executed");
+				resultString = Integer.toString(rsUser) + "|" + Integer.toString(rsAcc);
+				con.close();
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
 		}
 		else if(requestCode == getNewID)
 		{
