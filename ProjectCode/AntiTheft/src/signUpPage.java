26f8b784885eda5e3dfc38739dbdae71f2b6c2f6
Exodia <Exodia@10.24.28.176>
2019-12-02 17:15:26 -0600

    notes

--- ProjectCode/AntiTheft/src/signUpPage.java
+++ ProjectCode/AntiTheft/src/signUpPage.java
@@ -84,114 +84,130 @@ public class signUpPage extends User {
 		frame.setBounds(1000, 600, 1000, 750);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//first name box
 		firstNameTextbox = new JTextField();
 		firstNameTextbox.setBounds(196, 205, 171, 32);
 		firstNameTextbox.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		firstNameTextbox.setColumns(10);
 		
+		//phone text box
 		phoneTextbox = new JTextField();
 		phoneTextbox.setBounds(396, 399, 171, 32);
 		phoneTextbox.setColumns(10);
 		
+		//first name label
 		JLabel firstNameLabel = new JLabel("First name");
 		firstNameLabel.setBounds(196, 238, 171, 23);
 		firstNameLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		firstNameLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		firstNameLabel.setForeground(SystemColor.text);
 		
+		//Email label
 		JLabel emailLabel = new JLabel("Email");
 		emailLabel.setBounds(196, 430, 171, 23);
-
 		emailLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-
 		emailLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		emailLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-
 		emailLabel.setForeground(SystemColor.text);
 		
+		//pasword label
 		JLabel passwordLabel = new JLabel("Password");
 		passwordLabel.setBounds(592, 430, 171, 23);
 		passwordLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		passwordLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		passwordLabel.setForeground(SystemColor.text);
 		
+		
+		//Phone label
 		JLabel phoneLabel = new JLabel("Phone");
 		phoneLabel.setBounds(396, 430, 171, 23);
 		phoneLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		phoneLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		phoneLabel.setForeground(SystemColor.text);
 		
+		//last name label
 		JLabel lastNameLabel = new JLabel("Last name");
 		lastNameLabel.setBounds(452, 238, 171, 23);
 		lastNameLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		lastNameLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lastNameLabel.setForeground(SystemColor.text);
 		
+		//last name text field
 		lastNameTextbox = new JTextField();
 		lastNameTextbox.setBounds(452, 205, 171, 32);
 		lastNameTextbox.setColumns(10);
 		
+		//MI text field
 		txtA = new JTextField();
 		txtA.setBounds(394, 205, 30, 32);
 		txtA.setColumns(10);
 		
+		//MI label
 		JLabel middleInitialLabel = new JLabel("Mi");
 		middleInitialLabel.setBounds(394, 238, 30, 23);
 		middleInitialLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		middleInitialLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		middleInitialLabel.setForeground(SystemColor.text);
 		
+		//email text box
 		emailTextbox = new JTextField();
 		emailTextbox.setBounds(196, 399, 171, 32);
 		emailTextbox.setColumns(10);
 		
+		//street text box
 		streetAddressTexBox = new JTextField();
 		streetAddressTexBox.setBounds(196, 303, 171, 32);
 		streetAddressTexBox.setColumns(10);
 		
+		//address label
 		JLabel streetAddressLabel = new JLabel("Street Address");
 		streetAddressLabel.setBounds(196, 334, 171, 23);
-
 		streetAddressLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-
 		streetAddressLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		streetAddressLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-
 		streetAddressLabel.setForeground(SystemColor.text);
 		
+		//City label
 		JLabel cityLabel = new JLabel("City");
 		cityLabel.setBounds(396, 334, 161, 23);
 		cityLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		cityLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		cityLabel.setForeground(SystemColor.text);
 		
+		//city text box
 		cityTextbox = new JTextField();
 		cityTextbox.setBounds(396, 303, 171, 32);
 		cityTextbox.setColumns(10);
 		
+		//State label
 		JLabel stateLabel = new JLabel("State");
 		stateLabel.setBounds(592, 334, 85, 23);
 		stateLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		stateLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		stateLabel.setForeground(SystemColor.text);
 		
+		//state box
 		stateTextbox = new JTextField();
 		stateTextbox.setBounds(592, 303, 85, 32);
 		stateTextbox.setColumns(10);
 		
+		//zip code label
 		JLabel zipCodeLabel = new JLabel("Zip Code");
 		zipCodeLabel.setBounds(699, 334, 85, 23);
 		zipCodeLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		zipCodeLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		zipCodeLabel.setForeground(SystemColor.text);
 		
+		//zip field
 		zipCodeTextbox = new JTextField();
 		zipCodeTextbox.setBounds(699, 303, 85, 32);
 		zipCodeTextbox.setColumns(10);
 		
+		//pasword text field
 		passwordTextbox1 = new JTextField();
 		passwordTextbox1.setBounds(592, 399, 171, 32);
 		
+		//back button push
 		button = new JButton("Back");
 		button.setBounds(383, 587, 100, 32);
 		button.addActionListener(new ActionListener() {
@@ -201,10 +217,11 @@ public class signUpPage extends User {
 				openingScreen.setVisible(true);	
 			}
 		});
+		
+		//back button pushed
 		button.setForeground(Color.BLACK);
 		button.setFont(new Font("Tahoma", Font.BOLD, 15));
-		button.setBackground(Color.WHITE);
-		
+		button.setBackground(Color.WHITE);		
 		btnSignUp = new JButton("Sign up");
 		btnSignUp.setBounds(513, 587, 100, 32);
 		btnSignUp.addActionListener(new ActionListener() {
@@ -245,14 +262,19 @@ public class signUpPage extends User {
 				
 			}
 		});
+		
+		//Sighn up button
 		btnSignUp.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnSignUp.setBackground(Color.WHITE);
+		btnSignUp.setBackground(Color.WHITE);	
+		//sign up label
 		
 		lblSighnUp = new JLabel("Sign Up");
 		lblSighnUp.setBounds(394, 45, 189, 56);
 		lblSighnUp.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSighnUp.setForeground(Color.WHITE);
 		lblSighnUp.setFont(new Font("Impact", Font.PLAIN, 50));
+		
+		//Get info
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(firstNameTextbox);
 		frame.getContentPane().add(phoneTextbox);
